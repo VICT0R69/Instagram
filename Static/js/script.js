08e689b594cb58b25d7160b2c6e5f6a821379570
unknown <siddiquirizwan6969@gmail.com>
2023-01-07 19:46:08 +0530

    footer

--- Static/js/script.js
+++ Static/js/script.js
@@ -1,11 +1,11 @@
 let button = document.querySelector('.btn');
 let password = document.getElementById('password');
 password.addEventListener('input', () => {
-    if (password.value.length > 2) {
+    if (password.value.length > 5) {
         button.style.backgroundColor = 'rgb(0, 149, 246)';
         button.removeAttribute('disabled');
     } else {
-        button.style.backgroundColor = 'rgb(160, 215, 251)';
+        button.style.backgroundColor = 'rgb(84, 187, 255)';
         button.setAttribute('disabled', 'disabled');
 
     }
